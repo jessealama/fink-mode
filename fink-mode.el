b212ff2615ab75f5c78a1e7735c83f8b7527134b
Jesse Alama <jesse.alama@gmail.com>
2014-09-11 15:21:06 +0200

    UseMaxBuildJobs.

--- fink-mode.el
+++ fink-mode.el
@@ -88,7 +88,7 @@
     "SplitOff4" "SplitOff5" "Files" "PreInstScript" "PostInstScript"
     "PreRmScript" "PostRmScript" "ConfFiles" "InfoDocs" "DaemonicFile"
     "DaemonicName" "Homepage" "DescDetail" "DescUsage" "DescPackaging"
-    "DescPort" "DefaultScript")
+    "DescPort" "DefaultScript" "UseMaxBuildJobs")
   "List of Fink fields")
 
 (defconst fink-field-regexp
